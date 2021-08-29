416ddcd9a2145ab91c8a5f6317eb741a06d5f8c2
Jannik Arndt <jannik@jannikarndt.de>
2021-08-29 16:12:28 +0200

    Rename to SwiftUICharts

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftui-charts",
+    name: "SwiftUICharts",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        .library(name: "swiftui-charts", targets: ["swiftui-charts"])
+        .library(name: "SwiftUICharts", targets: ["SwiftUICharts"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-extensions/swiftui-shapes.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        .target(name: "swiftui-charts",dependencies: ["Shapes"])
+        .target(name: "SwiftUICharts",dependencies: ["Shapes"])
     ]
 )
